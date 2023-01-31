c469539f30b91e2300c0df5b298403f2dceab4ff
neyajahmad <neyajahmad8657@gmail.com>
2023-01-31 20:17:48 +0530

    End of Section 2

---
+++ NZWalks/NZWalks.API/Models/Domain/Region.cs
@@ -0,0 +1,13 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public String Code { get; set; }
+        public String Name { get; set; }
+        public double Area { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+        public long Populattion { get; set; }
+    }
+}
